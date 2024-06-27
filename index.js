f3778a3476fd9489e6e795f0e91aa836061bdccb
Kishan <kishanpatel968@gmail.com>
2024-06-27 17:52:41 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const heading = "Hello World";
+const root = document.getElementById("root");
+root.append(heading);
+
+const reactHeading = React.createElement("h1",{color: 'blue'},"hello from react");
+const appRoot = ReactDOM.createRoot(root);
+appRoot.render(reactHeading);
